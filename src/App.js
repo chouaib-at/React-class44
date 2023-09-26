007537ee90f64074f75871fd941fe2dde8548723
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2023-09-26 21:57:29 +0200

    week2

--- src/App.js
+++ src/App.js
@@ -1,83 +1,16 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import Categories from "./fake-data/all-categories";
-import Products from "./fake-data/all-products";
-
-function App() {
-  const [selectedCategory, setSelectedCategory] = useState(null);
-  const [productList, setProductList] = useState(Products);
-
-  useEffect(() => {
-    if (selectedCategory) {
-      setProductList(
-        Products.filter((product) =>
-          product.category.includes(selectedCategory)
-        )
-      );
-    }
-  }, [selectedCategory]);
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Product from "./Components/Product";
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <h1 className="cats">Categories</h1>
-        <div className="categoryList">
-          {Categories.map((c) => (
-            <button
-              key={c}
-              onClick={(e) => {
-                setSelectedCategory(c);
-              }}
-              className={
-                selectedCategory === c ? "category active" : "category"
-              }
-            >
-              {c}
-            </button>
-          ))}
-        </div>
-      </div>
-      <div>
-        <h1>Products</h1>
-        <div
-          style={{
-            display: "flex",
-            flexWrap: "wrap",
-            width: "100vw",
-            gap: "10px",
-          }}
-        >
-          {productList.map((product) => (
-            <div key={product.id} style={productContainerStyle}>
-              <img src={product.image} alt="product" style={imageStyle} />
-              <p style={nameStyle}>{product.title}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productId" element={<Product />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
-
-const productContainerStyle = {
-  border: "1px solid #ccc",
-  padding: "10px",
-  borderRadius: "10px",
-  textAlign: "center",
-  transition: "box-shadow 0.3s ease-in-out",
-};
-
-productContainerStyle[":hover"] = {
-  boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
-};
-
-const imageStyle = {
-  width: "200px",
-  height: "200px",
-};
-
-const nameStyle = {
-  fontWeight: "bold",
-  marginTop: "10px",
-};
---
+++ src/Components/Product.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import {useParams} from 'react-router-dom'
+const Product = () => {
+    const params = useParams(); 
+    console.log(params)
+    const productId=params.productId
+console.log(productId)
+const [product,setProduct]=useState()
+const [loading,setLoading]=useState(true)
+const [error,setError]=useState(false)
+useEffect(()=>{
+    const getProduct=async()=>{
+        try {
+            setLoading(true)
+            const response = await fetch(`https://fakestoreapi.com/products/${productId}`)
+            const product = await response.json();
+            console.log(product)
+            setProduct(product)
+            setLoading(false)
+        } catch (error) {
+            setError(true)
+            setLoading(false)
+        }
+    }
+    getProduct()
+},[])
+  return (
+    <div>
+        {loading?<p>Loading</p>:error?<p>error</p>:<div>
+            <h1>{product.title}</h1>
+            <h2>Description</h2>
+            <p>{product.description}</p>
+            <p>Price:{product.price}</p>
+            </div>
+            }
+    </div>
+  )
+}
+
+export default Product
